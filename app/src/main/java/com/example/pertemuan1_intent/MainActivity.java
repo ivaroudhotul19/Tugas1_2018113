cde371f76a5b9f5bf6e68919bae1b231e4245413
ivaroudhotul19 <ivaroudhotul@gmail.com>
2022-10-08 16:35:28 +0700

    pertemuan 1 intent

---
+++ app/src/main/java/com/example/pertemuan1_intent/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pertemuan1_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void biodata(View view){
+        Intent a = new Intent(MainActivity.this, Biodata.class);
+        startActivity(a);
+    }
+
+    public void segitiga(View view){
+        Intent a = new Intent(MainActivity.this, Segitiga.class);
+        startActivity(a);
+    }
+}
